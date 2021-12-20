9b401d5bb9f7302bc76d6ac41f8b41dd9f80d5c3
KorolkovAlexander <arhontos2@gmail.com>
2021-12-20 17:43:51 +0300

    's'

---
+++ pages/invalids/[id].js
@@ -0,0 +1,77 @@
+import { createClient } from "contentful";
+import Image from "next/image";
+import styles from "../../styles/post.module.css";
+import Sidebar from "../../Components/Sidebar";
+import { useState } from "react";
+import Footer from "../../Components/Footer";
+import { useRouter } from "next/router";
+import HeadSite from "../../Components/HeadSite";
+
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+});
+
+/* const Paths = async () => {
+  const res = await client.getEntries({
+    content_type: "posts"
+  });
+
+  const paths = res.items.map((item) => {
+    return {
+      params: { id: item.fields.id },
+    };
+  });
+
+  return {
+    paths,
+    fallback: true,
+  };
+};  */
+
+export const getServerSideProps = async (context) => {
+  const { id } = context.query;
+  const res = await client.getEntries({ content_type: "posts" });
+  /* const paths = res.items.map((item) => {
+    return {
+      params: { id: item.fields.id },
+    };
+  }); */
+
+  const { items } = await client.getEntries({
+    content_type: "invalids",
+    "fields.id": id,
+  });
+
+  return {
+    props: { posts: items[0] },
+    /*  revalidate: 1, */
+  };
+};
+export default function InvalidsId({ posts }) {
+  const [state, setState] = useState(false);
+
+  const updateData = (value) => {
+    setState(value);
+  };
+console.log(posts)
+  return (
+    <div>
+      <HeadSite />
+      <Sidebar tumb={state} updateData={updateData} />
+      <div className={state ? styles.wrap : styles.wrap2}>
+        <div key={posts.id} className={styles.image}>
+          <Image
+            src={"https:" + posts.fields.images.fields.file.url}
+            width={posts.fields.images.fields.file.details.image.width}
+            height={posts.fields.images.fields.file.details.image.height}
+          />
+        </div>
+        <h1 className={styles.title}>{posts.fields.title}</h1>
+         <div className={styles.desc}>
+          {posts.fields.description.content[0].content[0].value}
+        </div> 
+      </div>
+    </div>
+  );
+}
