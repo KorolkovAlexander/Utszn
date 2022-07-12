c6598b4e8df80b5957f5a9576d1b0f391cbb0cd0
KorolkovAlexander <arhontos2@gmail.com>
2022-07-12 11:56:22 +0300

    allowance

--- Components/Desccard.js
+++ Components/Desccard.js
@@ -3,11 +3,11 @@ import styles from "../styles/desccard.module.css";
 
 
 export default function Desccard({post}) {
-    
+    console.log(post)
    
     return (
         <div className={styles.container}>
-            {post.text.text}
+          <div className={styles.text}> {post.text} </div> 
             <br/>
         </div>
     )
